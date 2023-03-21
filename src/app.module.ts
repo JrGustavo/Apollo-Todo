e59003fa1dcdf46fa05c7badfaa903fcc6d261c3
JrGustavo <gus@soypetal.com>
2023-03-21 14:38:29 -0500

    Update Apollo

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,18 @@ import {ApolloDriver, ApolloDriverConfig} from "@nestjs/apollo";
 import { HelloworldModule } from './helloworld/helloworld.module';
 import { HelloWorldResolver } from './hello-world/hello-world.resolver';
 
+import {ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+
 @Module({
-  imports: [
-      GraphQLModule.forRoot<ApolloDriverConfig>({
-        driver: ApolloDriver,
-        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      }),
-      HelloworldModule,
-  ],
-  controllers: [],
-  providers: [HelloWorldResolver],
+    imports: [
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+            playground: false,
+            plugins: [ApolloServerPluginLandingPageLocalDefault()],
+        }),
+        HelloworldModule,
+    ],
+    providers: [HelloWorldResolver],
 })
 export class AppModule {}
--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,10 +1,21 @@
-import {Query, Resolver} from '@nestjs/graphql';
+import {Args, Float, Int, Query, Resolver} from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
 
-    @Query(() => String )
+    @Query(() => String, {description: 'Hola mundo es lo que retorna', name: 'hello'} )
     helloWorld (): string{
         return 'Hola Mundo';
     }
+
+@Query ( () => Float, { name: 'randomNumber'})
+getRandomNumber(): number {
+    return Math.random() *100;
+    }
+
+    @Query (() => Int, { name: 'randmonFromZeroTo', description: 'From Zero to argument To'})
+    getRandmonFromZeroTo( @Args('to', {type: () => Int}) to: number ): number {
+        return Math.floor(Math.random() *10);
+    }
+
 }
